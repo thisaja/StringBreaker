b78701d2f2216713d83e4a79c962fac84b8c2dad
Thisaja Alwis <thisajaalwis@gmail.com>
2019-11-18 12:16:12 -0500

    pretty much done
    just need to add the rules in and proofread

--- src/stringbreaker/StringBreaker.java
+++ src/stringbreaker/StringBreaker.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package stringbreaker;
 import java.util.*;
-import java.io.*;
 /**
  *
  * @author thisajaalwis
@@ -15,26 +9,46 @@ public class StringBreaker {
     /**
      * @param args the command line arguments
      */
+	private static boolean isValid(String s,int length) {
+		if(s.length()!=length)return false;
+		for(int i=0;i<s.length();i++)if(s.charAt(i)>=70)return false;
+		return true;
+	}
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        int chars,guesses;
+        StringMaker randomString;
         String guess;
+        int chars,guesses,correct,numCorrect;
+        boolean isCorrect=false;
         
         do{
-        System.out.print("Enter how many letters you want to guess(3,4 or 5): ");
-        chars=input.nextInt();
+	        System.out.print("Enter how many letters you want to guess(3,4 or 5): ");
+	        chars=input.nextInt();
         }while(chars<3 || chars>5);
-        StringMaker s=new StringMaker(chars);
-        System.out.println(s.getString());
+        
+        randomString=new StringMaker(chars);
+        System.out.println(randomString.getString());
         
         System.out.print("Enter how many times you want to guess: ");
         guesses=input.nextInt();
         
         for(int i=0;i<guesses;i++){
-            System.out.print("Enter your guess: ");
-            guess=input.next();
-            System.out.println(s.numCorrect(guess));
+        	System.out.println("");
+        	do {
+	            System.out.print("Enter your guess: ");
+	            guess=input.next().toUpperCase();
+        	}while(!isValid(guess,randomString.getLength()));
+        	numCorrect=randomString.numCorrectLetter(guess);
+        	correct=randomString.numCorrectPos(guess);
+            System.out.println(numCorrect+" of the letters you guessed were right and "+correct+" were in the right position.");
+            if(correct==randomString.getLength()) {
+            	isCorrect=true;
+            	break;
+            }
         }
+        input.close();
+        if(isCorrect)System.out.println("Good Job! You guessed right!");
+        else System.out.println("Nice Try! The correct answer was "+randomString.getString());
         
     }
     
--- src/stringbreaker/StringMaker.java
+++ src/stringbreaker/StringMaker.java
@@ -1,37 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package stringbreaker;
-
-import java.util.Arrays;
 
 /**
  *
  * @author thisajaalwis
  */
 public class StringMaker {
-    private String s="AAC";
-    private int length,min=65,max=69,range=(69 - 65) + 1;
+    private String s="";
+    private int length, min=65,max=69,range=(max - min) + 1;
     public StringMaker(int length){ 
         this.length=length;
-//        for(int i=0;i<length;i++){
-//            int num=(int)(Math.random() * range) + min;
-//            s+=(char)num;
-//        }
+        for(int i=0;i<length;i++){
+            int num=(int)(Math.random()*range) + min;
+            s+=(char)num;
+        }
     }
-    public String getString(){
-        return s;
+    public String getString() {
+    	return s;
     }
-    public int correct(String guess){
+    public int getLength() {
+    	return length;
+    }
+    public int numCorrectPos(String guess){
         int cnt=0;
         for(int i=0;i<length;i++){
             if(s.charAt(i)==guess.charAt(i))cnt++;
         }
         return cnt;
     }
-    public int numCorrect(String guess){
+    public int numCorrectLetter(String guess){
         int stringCnt[]=new int[5],guessCnt[]=new int[5],cnt=0;
         for(int i=0;i<length;i++){
             stringCnt[s.charAt(i)-65]++;
