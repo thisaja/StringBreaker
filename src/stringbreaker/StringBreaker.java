bdee0cd54a2ea792053f2aa26de480f1eb439832
itslinotlie <michael.li.web@gmail.com>
2019-11-19 10:26:20 -0500

    Finished up the project

--- src/stringbreaker/StringBreaker.java
+++ src/stringbreaker/StringBreaker.java
@@ -1,31 +1,39 @@
+/*
+Programmers: Thisaja Alwis and Michael Li
+Date: Novemeber 19, 2019
+Course Code: ICS3U
+File Name: StringBreaker.java
+Description: Application allows user to play a game
+*/
 package stringbreaker;
 import java.util.*;
 /**
- *
- * @author thisajaalwis
+ * Class Description: Client code, lets user decide various parameters
  */
 public class StringBreaker {
 
     /**
-     * @param args the command line arguments
+     * Method Description: Checks if the user input is valid or not
+     * Pre: A String and an integer
+     * Post: Returns a true/false value
      */
-	private static boolean isValid(String s,int length) {
-		if(s.length()!=length)return false;
-		for(int i=0;i<s.length();i++)if(s.charAt(i)>=70)return false;
-		return true;
-	}
+    private static boolean isValid(String s,int length) {
+            if(s.length()!=length)return false;
+            for(int i=0;i<s.length();i++)if(s.charAt(i)>=70)return false;
+            return true;
+    }
 	
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         StringMaker randomString;
         String guess;
-        int chars,guesses,correct,numCorrect;
-        boolean isCorrect=false;
+        int chars, guesses, correct, numCorrect;
+        boolean isCorrect = false;
         
-        do{
-	        System.out.print("Enter how many letters you want to guess(3,4 or 5): ");
-	        chars=input.nextInt();
-        }while(chars<3 || chars>5);
+        do {
+            System.out.print("Enter how many letters you want to guess (3, 4 or 5): ");
+            chars=input.nextInt();
+        } while (chars<3 || chars>5);
         
         randomString=new StringMaker(chars);
         
@@ -33,28 +41,24 @@ public class StringBreaker {
         guesses=input.nextInt();
         
         for(int i=0;i<guesses;i++){
-        	System.out.println("");
+            System.out.println("");
+
+            do {
+                System.out.print("Enter your guess: ");
+                guess=input.next().toUpperCase();
+            } while(!isValid(guess,randomString.getLength()));
         	
-        	do {
-	            System.out.print("Enter your guess: ");
-	            guess=input.next().toUpperCase();
-        	}while(!isValid(guess,randomString.getLength()));
-        	
-        	numCorrect=randomString.numCorrectLetter(guess);
-        	correct=randomString.numCorrectPos(guess);
+            numCorrect=randomString.numCorrectLetter(guess);
+            correct=randomString.numCorrectPos(guess);
             System.out.println(numCorrect+" of the letters you guessed were right and "+correct+" were in the right position.");
             
             if(correct==randomString.getLength()) {
             	isCorrect=true;
             	break;
-            }
-            
+            }   
         }
-        
         input.close();
-        if(isCorrect)System.out.println("Good Job! You guessed right!");
-        else System.out.println("Nice Try! The correct answer was "+randomString.getString()+".");
-        
-    }
-    
+        if(isCorrect)System.out.println("Good Job! You guessed the string!!!");
+        else System.out.println("Nice Try! The correct answer was "+randomString.getString()+".");   
+    }  
 }
--- src/stringbreaker/StringMaker.java
+++ src/stringbreaker/StringMaker.java
@@ -1,45 +1,76 @@
+/*
+Programmers: Thisaja Alwis and Michael Li
+Date: Novemeber 19, 2019
+Course Code: ICS3U
+File Name: StringMaker.java
+*/
 package stringbreaker;
 
 /**
- *
- * @author thisajaalwis
+ * Class Description: Creates a random string 
  */
 public class StringMaker {
     private String randomString="";
-    private int length, min=65,max=69,range=(max - min) + 1;
+    private final int MIN=65,MAX=69,RANGE=(MAX - MIN) + 1,LENGTH;
     
+    /**
+     * Method Description: Creates the random string based on the length required
+     * Pre: An integer value
+     * Post: None
+     */
     public StringMaker(int length){ 
-        this.length=length;
+        this.LENGTH=length;
         for(int i=0;i<length;i++){
-            int num=(int)(Math.random()*range) + min;
+            double num=(int)(Math.random()*RANGE) + MIN;
             randomString+=(char)num;
         }
     }
     
+    /**
+     * Method Description: Returns the generated string
+     * Pre: None
+     * Post: The string 
+     */
     public String getString() {
     	return randomString;
     }
     
+    /**
+     * Method Description: Returns the length of the generated string
+     * Pre: None
+     * Post: The integer length of the string 
+     */
     public int getLength() {
-    	return length;
+    	return LENGTH;
     }
     
+    /**
+     * Method Description: Checks the user's input against the generated string to find the number
+     *                     of letters that are in the correct position
+     * Pre: Users string guess
+     * Post: An integer displaying the number of correct letters in the right position
+     */
     public int numCorrectPos(String guess){
         int cnt=0;
-        for(int i=0;i<length;i++){
+        for(int i=0;i<LENGTH;i++){
             if(randomString.charAt(i)==guess.charAt(i))cnt++;
         }
         return cnt;
     }
     
+    /**
+     * Method Description: Checks the user's input against the generated string to find the number
+     *                     of unique letters that are correct
+     * Pre: Users string guess
+     * Post: An integer displaying the number of unique letters that are in the generated string
+     */
     public int numCorrectLetter(String guess){
         int stringCnt[]=new int[5],guessCnt[]=new int[5],cnt=0;
-        for(int i=0;i<length;i++){
+        for(int i=0;i<LENGTH;i++){
             stringCnt[randomString.charAt(i)-65]++;
             guessCnt[guess.charAt(i)-65]++;
         }
         for(int i=0;i<5;i++)cnt+=Math.min(stringCnt[i], guessCnt[i]);
         return cnt;
     }
-    
 }
